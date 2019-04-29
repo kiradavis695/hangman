8ec64a35fe6bfe906d875944fd2e0bdbcd1cd0b3
subarnajoshi <43258371+subarnajoshi@users.noreply.github.com>
2019-04-29 13:21:56 -0600

    Update hangman.rb

--- hangman.rb
+++ hangman.rb
@@ -97,9 +97,16 @@ until h.misses == 6
   # Check if letter is in word
     # Yes - change _ to letter
     # No - increment misses
+  for i in 1..h.length
   if h.word include input
-
-
+    if i == h.word.index(input) + 1
+        print input
+    else
+        print "_"
+    end
+  else 
+      print "_"
+  end
   # ask for input again
   # Check if user has lost
 
